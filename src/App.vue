518fdeb8e848444b4fc2b8007da1141f1321ac22
paturgtg123 <fathur0678@gmail.com>
2024-05-14 18:50:41 +0700

    penambahan

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,67 @@
 <template>
+  <header>
+    <nav>
+      <ul class="menu">
+        <li :class="{ active: currentView === 'todos' }" @click="currentView = 'todos'">Todos</li>
+        <li :class="{ active: currentView === 'posts' }" @click="currentView = 'posts'">Posts</li>
+      </ul>
+    </nav>
+  </header>
   <div class="container">
-    <h1>Daftar Lagu Favorit</h1>
+    <div v-if="currentView === 'todos'">
+      <h1>Daftar Lagu Favorit</h1>
 
-    <form class="form" @submit.prevent="tambahLagu">
-      <input class="input" v-model="laguBaru" type="text" placeholder="Tambahkan lagu baru">
-      <button class="button" type="submit">Tambah</button>
-    </form>
+      <form class="form" @submit.prevent="tambahLagu">
+        <input class="input" v-model="laguBaru" type="text" placeholder="Tambahkan lagu baru">
+        <button class="button" type="submit">Tambah</button>
+      </form>
 
-    <ul class="lagu-list">
-      <li v-for="(lagu, index) in laguTidakSelesai" :key="index" class="lagu-item">
-        <input class="checkbox" type="checkbox" v-model="lagu.selesai">
-        <span class="nama-lagu" :class="{ 'selesai': lagu.selesai }">{{ lagu.nama }}</span>
-        <button class="button batalkan-button" @click="hapusLagu(index)">Hapus</button>
-      </li>
-    </ul>
+      <ul class="lagu-list">
+        <li v-for="(lagu, index) in laguTidakSelesai" :key="index" class="lagu-item">
+          <input class="checkbox" type="checkbox" v-model="lagu.selesai">
+          <span class="nama-lagu" :class="{ 'selesai': lagu.selesai }">{{ lagu.nama }}</span>
+          <button class="button batalkan-button" @click="hapusLagu(index)">Hapus</button>
+        </li>
+      </ul>
 
-    <label class="filter-label">
-      <input class="filter-checkbox" type="checkbox" v-model="filterSelesai"> Tampilkan yang Belum Selesai
-    </label>
+      <label class="filter-label">
+        <input class="filter-checkbox" type="checkbox" v-model="filterSelesai"> Tampilkan yang Belum Selesai
+      </label>
+    </div>
+
+    <div v-if="currentView === 'posts'" class="fetch-section">
+      <div v-if="users.length" class="result">
+        <h2>Users:</h2>
+        <select v-model="selectedUser" @change="filterPosts">
+          <option value="">All Users</option>
+          <option v-for="user in users" :key="user.id" :value="user.id">{{ user.name }}</option>
+        </select>
+      </div>
+
+      <div v-if="selectedUser && filteredPosts.length" class="result">
+        <h2>Posts:</h2>
+        <ul>
+          <li v-for="post in filteredPosts" :key="post.id">{{ post.title }}</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
+      currentView: 'todos',
       laguBaru: '',
       laguList: [],
-      filterSelesai: false
+      filterSelesai: false,
+      users: [],
+      posts: [],
+      selectedUser: '',
+      filteredPosts: []
     };
   },
   methods: {
@@ -49,6 +83,32 @@ export default {
       if (this.filterSelesai) {
         this.laguList = this.laguList.filter(lagu => !lagu.selesai);
       }
+    },
+    async fetchData() {
+      try {
+        const [usersResponse, postsResponse] = await Promise.all([
+          axios.get('https://jsonplaceholder.typicode.com/users'),
+          axios.get('https://jsonplaceholder.typicode.com/posts')
+        ]);
+        this.users = usersResponse.data;
+        this.posts = postsResponse.data;
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    },
+    filterPosts() {
+      if (this.selectedUser) {
+        this.filteredPosts = this.posts.filter(post => post.userId === parseInt(this.selectedUser));
+      } else {
+        this.filteredPosts = [];
+      }
+    }
+  },
+  watch: {
+    currentView(newView) {
+      if (newView === 'posts' && this.users.length === 0 && this.posts.length === 0) {
+        this.fetchData();
+      }
     }
   },
   computed: {
@@ -56,7 +116,7 @@ export default {
       if (this.filterSelesai) {
         return this.laguList.filter(lagu => !lagu.selesai);
       } else {
-        return this.laguList.filter(lagu => lagu.selesai);
+        return this.laguList;
       }
     }
   }
@@ -64,20 +124,47 @@ export default {
 </script>
 
 <style>
-.container {
-  border :2px solid aqua;
-  background-color: rgba(225, 225, 225, 0.4);
-  background-size:cover;
-  box-shadow: 10px 10px 20px rgb(53, 50, 50);
-  margin:auto;
-  width: 500px;
-  padding: 25px;
-  border-radius: 10px;
-  border-color:rgb(237, 237, 237);
-}
+
 body {
   background-image: url('./assets/bamboo.jpg');
-  background-size:cover;
+  background-size: cover;
+}
+
+header {
+  width: 100%;
+  text-align: center;
+  left: 0;
+  top: 0;
+  position:absolute;
+}
+
+nav {
+  background-color: #459848;
+  width: 100%;
+  display: inline-block;
+}
+
+.menu {
+  list-style-type: none;
+  padding: 0;
+  display: flex;
+  justify-content:center;
+}
+
+.menu li {
+  cursor: pointer;
+  padding: 10px;
+  background-color: #459848;
+  color: white;
+  border-radius: 4px;
+}
+
+.menu li.active {
+  background-color: #459848;
+}
+
+.menu li:hover {
+  background-color: #459848;
 }
 
 .form {
@@ -112,14 +199,14 @@ body {
 }
 
 .lagu-item {
-    display: flex;
-    justify-content: space-between; 
-    align-items: center; 
-    background-color: rgba(44, 42, 42, 0.8);
-    padding: 10px;
-    border-radius: 4px;
-    margin-bottom: 10px;
-  }
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: rgba(44, 42, 42, 0.8);
+  padding: 10px;
+  border-radius: 4px;
+  margin-bottom: 10px;
+}
 
 .checkbox {
   margin-right: 10px;
@@ -138,12 +225,42 @@ body {
 }
 
 .filter-label {
-  display:inline-block;
+  display: inline-block;
   margin-top: 20px;
   font-size: 16px;
 }
 
 .filter-checkbox {
   margin-right: 5px;
+}
+
+.fetch-section {
+  margin-top: 20px;
+}
+
+.fetch-button {
+  margin-right: 10px;
+  background-color: #2196F3;
+}
+
+.fetch-button:hover {
+  background-color: #0b7dda;
+}
+
+.result {
+  margin-top: 20px;
+}
+
+.result h2 {
+  margin-bottom: 10px;
+}
+
+.result ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+.select {
+  margin-bottom: 20px;
 }
 </style>
